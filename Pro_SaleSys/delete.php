945f474329f7f54ec77dee25361782dd2b159f0a
luoqing <852925993@qq.com>
2016-10-15 17:19:25 +0800

    rm

--- Pro_SaleSys/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "mysql_tool.php";
-if(isset($_POST['id'])){
-	$mysqlTool=new MysqlTool;
-	$mysqlTool->deleteById($_POST['id']);
-	
-}
-
-?>
--- Pro_SaleSys/filelist.php
+++
@@ -1,8 +0,0 @@
-<?php
-$filelist=array(
-	'manage_login'=>'manage_login.php',
-	'manage_index'=>"manage_index.php",
-	'manage_request_process'=>'request_process.php'
-	);
-
-?>
